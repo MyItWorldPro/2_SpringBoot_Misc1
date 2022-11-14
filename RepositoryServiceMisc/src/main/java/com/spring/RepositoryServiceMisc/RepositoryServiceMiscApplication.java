0e143bf85183dadbbd104b8bb5ef43db59dad105
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2022-11-14 15:28:30 +0530

    initial commit

---
+++ RepositoryServiceMisc/src/main/java/com/spring/RepositoryServiceMisc/RepositoryServiceMiscApplication.java
@@ -0,0 +1,13 @@
+package com.spring.RepositoryServiceMisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryServiceMiscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryServiceMiscApplication.class, args);
+	}
+
+}
---
+++ RepositoryServiceMisc/src/test/java/com/spring/RepositoryServiceMisc/RepositoryServiceMiscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.RepositoryServiceMisc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryServiceMiscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
