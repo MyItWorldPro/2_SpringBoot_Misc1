2f8107b67f4db36c17d5030d0ae23857a51a3c88
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2022-12-09 11:11:39 +0530

    initial commit

---
+++ RequestValidation/src/test/java/com/springboot/RequestValidation/RequestValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.RequestValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestValidationApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
