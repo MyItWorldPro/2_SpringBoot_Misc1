21cee3289ab425776f4212bdd46560890ae43358
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2022-12-27 15:22:55 +0530

    initial commit

---
+++ TransactionMgmt/src/main/java/com/misc/TransactionMgmt/TransactionMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.misc.TransactionMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionMgmtApplication.class, args);
+	}
+
+}
